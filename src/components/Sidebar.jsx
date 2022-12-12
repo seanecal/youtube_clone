f48011dd846e807760a335e92fb610b6491719eb
Sen Cao <caosen.cn@gmail.com>
2022-12-12 23:53:03 +0200

    added sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,10 @@
+import { Stack } from '@mui/material'
+import { categories } from '../utilities/constants'
+
 export function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <Stack direction="row" sx={{overflowY: "auto", height: {sx:'auto', md: '95%', flexDirection: {md: "column"}}}}>
+
+    </Stack>
   )
 }
