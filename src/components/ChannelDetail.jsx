142b983c58057497116ef693f24b0d4ef19b78de
Sen Cao <caosen.cn@gmail.com>
2022-12-23 23:55:00 +0200

    channel page logic implemented

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,5 +1,25 @@
+import { Box } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from '../utilities/fetchFromAPI'
+import { Video, ChannelCard} from './'
+
 export function ChannelDetail() {
+  const { id } = useParams()
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  useEffect(()=> {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data)=> setChannelDetail(data?.items[0]))
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data)=> setVideos(data?.items))
+  },[id])
+
+
   return(
-    <div>channelDetail</div>
+    <Box minHeight="95vh">
+
+    </Box>
   )
 }
